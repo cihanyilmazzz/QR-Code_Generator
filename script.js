21b3672ce0615eb7deba1819ed7402963d946dfd
Cihan Yılmaz <chnrzgr@gmail.com>
2024-09-27 15:21:48 +0200

    script update

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+// API URL
+var qrApiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
 
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+let gnrtBtn = document.getElementById("gnrt-btn");
 
+function generateQR() {
+    qrImage.src = qrApiUrl + qrText.value;
+}
 
-
+gnrtBtn.addEventListener("click", generateQR);
 
 
 
